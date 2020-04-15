5b0ca8b1edf514ffdc6ee8ef760be107cdf48f9f
laurenshareshian <laurenshareshian@gmail.com>
2020-04-15 13:53:34 -0700

    first

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,12 @@ from flask import Flask
 from config import Config
 # from flask_sqlalchemy import SQLAlchemy
 # from flask_migrate import Migrate
-from app import createdatabase
+from app.createdatabase import createdatabase
 
 app = Flask(__name__)
 app.config.from_object(Config)
 # db = SQLAlchemy(app)
 # migrate = Migrate(app, db)
-createdatabase.createdatabase()
+createdatabase()
 
 from app import routes, models
