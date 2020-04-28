d06a90078558617416838baa729ae21fe07cc833
laurenshareshian <laurenshareshian@gmail.com>
2020-04-27 23:31:32 -0700

    updated frontend to interact with sql

--- createdatabase.py
+++ createdatabase.py
@@ -70,7 +70,7 @@ def createdatabase(database_url):
        first_name        CHAR(50)     NOT NULL,
        last_name         CHAR(50)     NOT NULL,
        email         CHAR(50)     NOT NULL,
-       telephone         CHAR(50)     NOT NULL,
+       telephone         CHAR(50)     NOT NULL
     );
     '''
     cursor.execute(sql)
