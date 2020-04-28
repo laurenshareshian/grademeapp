68aee2d7e2458919cc537e72df884ee0359c2950
Joshua Cox <jnokoi@gmail.com>
2020-04-28 15:03:25 -0700

    update main to run server

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 from app import app
+
+import sys
+import os
+
+if __name__ == '__main__':
+    host = os.environ.get('HOST', '0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host=host, port=port)
