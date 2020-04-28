e104b99da08aa409eb47e2d7416ef9c7a7ef82ba
laurenshareshian <laurenshareshian@gmail.com>
2020-04-27 23:29:46 -0700

    updated frontend to interact with sql

--- createdatabase.py
+++ createdatabase.py
@@ -41,7 +41,7 @@ def createdatabase(database_url):
     #    LAST         CHAR(50)     NOT NULL,
     #    EMAIL        CHAR(50)
     # );
-    # '''
+    # # '''
     # cursor.execute(sql)
     # conn.commit()
     #
@@ -66,11 +66,11 @@ def createdatabase(database_url):
 
     cursor = conn.cursor()
     sql = '''CREATE TABLE teacher(
-       teacher_id        SERIAL PRIMARY KEY,
+       teacher_id SERIAL PRIMARY KEY,
        first_name        CHAR(50)     NOT NULL,
        last_name         CHAR(50)     NOT NULL,
-       email             CHAR(50)     NOT NULL,
-       telephone         CHAR(10)     NOT NULL
+       email         CHAR(50)     NOT NULL,
+       telephone         CHAR(50)     NOT NULL,
     );
     '''
     cursor.execute(sql)
